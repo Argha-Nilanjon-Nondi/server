391df38e600271131f102d5ef958790ebce9c66d
Argha Nilanjon Nondi <pcic095@gmail.com>
2023-07-08 10:06:01 +0600

    Almost done

--- atest.py
+++ atest.py
@@ -1,12 +1,6 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-import os
-from cryptography.hazmat.primitives.padding import PKCS7
- 
-h=[]
 
 def generate_key_pair():
     private_key = rsa.generate_private_key(
@@ -39,13 +33,12 @@ def save_key_to_file(key, filename):
 def load_key(filename):
     key_file=open(filename, "rb")
     if("public" in filename):
-       print("hi lol")
        key = serialization.load_pem_public_key(
            key_file.read(),
            backend=default_backend()
           )
     if("private" in filename):
-         key = serialization.load_pem_private_key(
+       key = serialization.load_pem_private_key(
             key_file.read(),
             password=None,
             backend=default_backend()
@@ -54,7 +47,7 @@ def load_key(filename):
     return key
 
 
-def encrypt_file(input_file, public_key):
+def encrypt_file(bytes_data, public_key):
     """
      Generate encrypted data 
    
@@ -67,47 +60,43 @@ def encrypt_file(input_file, public_key):
      
     """
   
-    chunk_size = 245 #bytes 
+    chunk_size = (public_key.key_size//8)-11 #bytes 
     final_chunk=b""
+    data=bytes_data
+    position=0
     
-    with open(input_file,"rb") as file:
-         while True:
-            chunk = file.read(chunk_size)
-            if(len(chunk)==0):
-               break
-            encrypted_chunk=public_key.encrypt(
-                          chunk,padding.PKCS1v15())
-            final_chunk+=encrypted_chunk
+    while True:
+      chunk = data[position:position+chunk_size]
+      position=position+chunk_size
+      if(len(chunk)==0):
+          break
+      encrypted_chunk=public_key.encrypt(
+                    chunk,padding.PKCS1v15())
+      final_chunk+=encrypted_chunk
             
-    encrypted_file=open("encrypted_content","ab")      
-    encrypted_file.write(final_chunk)
-    encrypted_file.close()
-    file.close()
-    
-    return True
+    return final_chunk
 
-def decrypt_file(encrypted_file,private_key):
+def decrypt_file(bytes_data,private_key):
     
-    chunk_size=256
+    chunk_size=private_key.key_size//8 #bytes
     final_chunk=b""
+    position=0
+    data=bytes_data
   
-    with open(encrypted_file,"rb") as encrypted_file:
-       while True:
-         chunk = encrypted_file.read(chunk_size)
-         if not chunk:
-            break
-         else:
-            decrypted_chunk = private_key.decrypt(
+    while True:
+      chunk = data[position:position+chunk_size]
+      position=position+chunk_size
+      if(len(chunk)==0):
+         break
+      else:
+         decrypted_chunk = private_key.decrypt(
                               chunk,
                               padding.PKCS1v15()
                                 )
-            final_chunk+=decrypted_chunk
+         final_chunk+=decrypted_chunk
             
     
-    decrypted_file=open("a9hhh9.png","ab")
-    decrypted_file.write(final_chunk)
-    encrypted_file.close()
-    decrypted_file.close()
+    return final_chunk
 
 # Generate the key pair
 #private_key, public_key = generate_key_pair()
@@ -118,13 +107,24 @@ def decrypt_file(encrypted_file,private_key):
 # Load the keys from files
 loaded_private_key = load_key('private_key.pem')
 loaded_public_key =  load_key('public_key.pem')
-print(loaded_public_key)
-# Encrypt and decrypt a file
-input_file = "006.jpg"
-encrypted_file = 'encrypted.txt'
-decrypted_file = 'decrypted.txt'
 
-print(encrypt_file(input_file, loaded_public_key))
-#decrypt_file(encrypted_file, decrypted_file, loaded_private_key)
-decrypt_file("encrypted_content",loaded_private_key)
-print("File encryption and decryption completed.")
+
+# Encrypt and decrypt a file
+input_filename = "006.jpg"
+encrypted_filename = 'encrypted.bin'
+decrypted_filename = 'shshsh.jpg'
+
+input_file=open(input_filename,"rb")
+input_content=input_file.read()
+encrypted_file=open(encrypted_filename,"wb")
+decrypted_file=open(decrypted_filename,"wb")
+
+encrypted_content=encrypt_file(input_content, loaded_public_key)
+encrypted_file.write(encrypted_content)
+
+decrypted_content=decrypt_file(encrypted_content,loaded_private_key)
+decrypted_file.write(decrypted_content)
+
+input_file.close()
+encrypted_file.close()
+decrypted_file.close()
